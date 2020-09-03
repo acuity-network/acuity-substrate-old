4e3e7100dfcbc74abf8a3b59fed4505fd44e2f64
Jonathan Brown <jbrown@mix-blockchain.org>
2020-09-03 22:12:35 +0700

    Set ACU claim message.

--- runtime/common/src/claims.rs
+++ runtime/common/src/claims.rs
@@ -64,9 +64,7 @@ impl StatementKind {
 	fn to_text(self) -> &'static [u8] {
 		match self {
 			StatementKind::Regular =>
-				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
-				Qmc1XYqT6S39WNp2UeiRUrZichUWUPpGEThDE6dAb3f6Ny. (This may be found at the URL: \
-				https://statement.polkadot.network/regular.html)"[..],
+				&b"I want to claim my ACU."[..],
 			StatementKind::Saft =>
 				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
 				QmXEkMahfhHJPzT3RjkXiZVFi77ZeVeuxtAjhojGRNYckz. (This may be found at the URL: \
